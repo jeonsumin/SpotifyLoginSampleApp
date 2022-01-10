69706535e1e29ec07b283a2d53a4ab29593afcbd
joensumin <soomis7@naver.com>
2022-01-10 11:08:37 +0900

    firebase 로그아웃 기능 구현

--- SpotifyLoginSampleApp/MainViewController.swift
+++ SpotifyLoginSampleApp/MainViewController.swift
@@ -34,7 +34,14 @@ class MainViewController: UIViewController {
     
     //MARK: - IBAction
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
-        self.navigationController?.popToRootViewController(animated: true)
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        }catch let signOutError as NSError {
+            print("ERROR: signout \(signOutError.localizedDescription)")
+        }
+
     }
     
 }
